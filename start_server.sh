3adab5463fb7e3d5f15da399c6d465c83ff8226a
Cellis <alexander@sellstrom.me>
2019-04-17 18:33:26 +0200

    Lade till skript för att starta servern lokalt.
    ./start_server.sh
    Glöm inte att byta till din IP i App.js rader 29 och 38.

---
+++ start_server.sh
@@ -0,0 +1,4 @@
+# Script for starting the node backend for PartyZone
+# 20190417 17:59
+echo "Starting server..."
+appKey=7f7b041e7c2f4c49996c8ada2c641ca7 appSecret=97028381128a462aa7ee9b439b74771f callbackURL="https://auth.expo.io/@rednaxela5950/spotify-react-native" node AwesomeProject/server.js &
