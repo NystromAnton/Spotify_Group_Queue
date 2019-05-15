ff23cb19a615515a4ee6fbf4ef0f6c41d4a09775
Cellis <alexander@sellstrom.me>
2019-05-15 12:47:53 +0200

    index.js updaterat

--- firebase_functions/functions/index.js
+++ firebase_functions/functions/index.js
@@ -13,26 +13,41 @@ admin.initializeApp(functions.config().firebase);
 exports.addRoom = functions.https.onCall((data, context) => {
     const room = admin.firestore().collection('rooms').doc(data.roomname);
     return room.set({
-        id: data["id"],
+        explicit: true,
+        voting: true,
+        genre: "none",
+        maxQueueSize: 1000,
+        songsPerPerson:100
     }, { merge: true });
 });
 
-exports.roomExists = functions.https.onCall((data, context) => {
-    admin.firestore().collection('rooms').doc(data.roomname).get()
-        .then((docSnapshot) => {
-            if (docSnapshot.exists) {
-		// do the things and send TRUE to app
-            } else {
-                // do nothing and send FALSE to app
-            }
-        }
-    }
-}
+// exports.roomExists = functions.https.onCall((data, context) => {
+//     admin.firestore().collection('rooms').doc(data.roomname).get()
+//         .then((docSnapshot) => {
+//             if (docSnapshot.exists) {
+// 		// do the things and send TRUE to app
+//             } else {
+//                 // do nothing and send FALSE to app
+//             }
+//         }
+//     }
+// }
 
 exports.addSong = functions.https.onCall((data, context) => {
-    const songs = admin.firestore().collection('rooms/ZzqG0nrtavCSfv2uxB53/songs');
+    const songs = admin.firestore().collection('rooms/' + data.roomname + '/songs');
     return songs.add({
         id: data["id"],
         votes: data["votes"]
     });
+});
+
+exports.editRoom = functions.https.onCall((data, context) => {
+    const room = admin.firestore().collection('rooms').doc(data.roomname);
+    return room.set({
+        explicit: data["explicit"],
+        voting: data["voting"],
+        genre: data["genre"],
+        maxQueueSize: data["maxQueueSize"],
+        songsPerPerson: data["songsPerPerson"]
+    }, { merge: true });
 });
