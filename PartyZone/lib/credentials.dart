63a9bce7c5b9c077da776135e5b90f6d03d42558
Cellis <alexander@sellstrom.me>
2019-05-21 10:42:54 +0200

    Backend ändringar. Inloggning kanske fungerar?

--- PartyZone/lib/credentials.dart
+++ PartyZone/lib/credentials.dart
@@ -2,6 +2,6 @@ class Credentials {
   static String clientId = "7f7b041e7c2f4c49996c8ada2c641ca7";
   static String redirectUrl = "spotify-flutter://callback";
   static String authToken = null;
-
+  static String hash = null;
   static String state = null;
 }
--- firebase_functions/functions/index.js
+++ firebase_functions/functions/index.js
@@ -1,5 +1,6 @@
 const functions = require('firebase-functions');
-
+const request = require('request');
+const crypto = require('crypto');
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
@@ -7,82 +8,69 @@ const functions = require('firebase-functions');
 //  response.send("Hello from Firebase!");
 // });
 
-exports.addUser = functions.https.onCall((data, context) => {
-    //skicka data.token till spotify
-    const request = require('request');
-
-    const options = {
-        url: 'https://api.spotify.com/v1/me',
-        headers: {
-            'User-Agent': 'request',
-            'Authorization': 'Bearer ' + data.token,
-            'Content-Type': 'application/json'
-        }
-    };
-
-    function callback(error, response, body) {
-        if (!error && response.statusCode === 200) {
-            const info = JSON.parse(body);
-            console.log(info);
-        }
-    }
-
-    request(options, callback);
-
-    var spotifyUser = request.JSON(id)
-    console.log(spotifyUser)
-    /*
-    //få username, spara till const spotifyUser
-    const hash = crypto.createHash("sha512").update(spotifyUser + "saltlakrits").digest('hex');
-    const user = admin.firestore().collection('users').doc(hash);
-    return user.set({
-        karma: 0,
-        voted: [],//array med låtar/dokument som man har röstat på.
-        added: []//array med låtar/dokument som man har lagt till.
-    }, {merge: true});*/
-});
-
-exports.addUser = functions.https.onCall((data, context) => {
-    //skicka data.token till spotify
-    const request = require('request');
-
-    const options = {
-        url: 'https://api.spotify.com/v1/me',
-        headers: {
-            'User-Agent': 'request',
-            'Authorization': 'Bearer ' + data.token,
-            'Content-Type': 'application/json'
-        }
-    };
-
-    function callback(error, response, body) {
-        if (!error && response.statusCode === 200) {
-            const info = JSON.parse(body);
-            console.log(info);
-        }
-    }
-
-    request(options, callback);
-
-    var spotifyUser = request.JSON(id)
-    console.log(spotifyUser)
-    /*
-    //få username, spara till const spotifyUser
-    const hash = crypto.createHash("sha512").update(spotifyUser + "saltlakrits").digest('hex');
-    const user = admin.firestore().collection('users').doc(hash);
-    return user.set({
-        karma: 0,
-        voted: [],//array med låtar/dokument som man har röstat på.
-        added: []//array med låtar/dokument som man har lagt till.
-    }, {merge: true});*/
-});
-
 const admin = require('firebase-admin');
 admin.initializeApp(functions.config().firebase);
 
+exports.addUser = functions.https.onCall((data, context) => {
+
+  const url = 'https://api.spotify.com/v1/me';
+  const options = {
+       url: 'https://api.spotify.com/v1/me',
+       headers: {
+           'User-Agent': 'request',
+           'Authorization': 'Bearer ' + data.token,
+           'Content-Type': 'application/json'
+       }
+   };
+
+    function downloadPage(options) {
+        console.log("options", options);
+        return new Promise((resolve, reject) => {
+            console.log("return new promise");
+            request(options, (error, response, body) => {
+                console.log("error, body here ---");
+                console.log(error, body);
+                if (error) reject(error);
+                if (response.statusCode !== 200) {
+                    reject(new Error('Invalid status code <' + response.statusCode + '>'));
+                }
+                resolve(body);
+            });
+        });
+    }
+
+    async function doit() {
+    try {
+        const body = await downloadPage(options);
+        console.log(body);
+        console.log("checkp 1")
+        const json = JSON.parse(body);
+        const spotifyUser = json.id;
+        console.log("checkp 2")
+        console.log(spotifyUser);
+        const hash = crypto.createHash("sha512").update(spotifyUser + "saltlakrits").digest('hex');
+        console.log("checkp 3")
+        const user = admin.firestore().collection('users').doc(hash);
+        console.log("--- user.set ---")
+        user.set({
+          karma: 0,
+          voted: [5, true, 'hello'],//array med låtar/dokument som man har röstat på.
+          added: [5, true, 'hello']//array med låtar/dokument som man har lagt till.
+        }, {merge: true});
+        console.log(hash)
+        return hash;
+      } catch (error){
+        console.error(error);
+        return {}
+      }
+    }
+    console.log("return doit")
+    return doit();
+});
+
 exports.addRoom = functions.https.onCall((data, context) => {
     const room = admin.firestore().collection('rooms').doc(data.roomname);
-    return room.set({
+    room.set({
         explicit: true,
         voting: true,
         genre: "none",
@@ -105,19 +93,38 @@ exports.addRoom = functions.https.onCall((data, context) => {
 
 exports.addSong = functions.https.onCall((data, context) => {
     const songs = admin.firestore().collection('rooms/' + data.roomname + '/songs');
-    return songs.add({
+    songs.add({
+        submitter: data["submitter"],
         id: data["id"],
+        image: data["image"],
+        title: data["title"],
+        artists: data["artists"],
         votes: data["votes"]
     });
 });
 
 exports.editRoom = functions.https.onCall((data, context) => {
     const room = admin.firestore().collection('rooms').doc(data.roomname);
-    return room.set({
+    room.set({
         explicit: data["explicit"],
         voting: data["voting"],
         genre: data["genre"],
         maxQueueSize: data["maxQueueSize"],
         songsPerPerson: data["songsPerPerson"]
     }, { merge: true });
+});
+
+
+exports.addVote = functions.https.onCall((data, context) => {
+    const song = admin.firestore().collection('rooms/' + data.roomname + '/songs').doc(data.song);
+    const user = admin.firestore().collection('users/' + data.hash);
+    const submitter = admin.firestore().collection('users/' + data.submitter)
+
+    if (data.upvote === true) {
+        const vote = admin.firestore.FieldValue.increment(1);
+    } else {
+        const vote = admin.firestore.FieldValue.increment(-1);
+    }
+    song.update({ votes: vote})
+    submitter.update({ karma: vote})
 });
